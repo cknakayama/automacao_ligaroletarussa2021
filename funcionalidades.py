50cf3ca5b01ec26b87d1f2e1ac0f1b33931a7b6b
cknakayama <ckn14oz@hotmail.com>
2021-05-20 13:56:43 -0300

    mais funcionalidades

--- funcionalidades.py
+++ funcionalidades.py
@@ -36,35 +36,72 @@ class Funcionalidades:
                 break
         return api
 
-    def listar_itens(self, dicionario):
-        c = 1
-        for item in dicionario:
-            print(f"{c}: ", end=" ")
-            for k,i in item.items():
-                print(f"{k} - {i}", end=" ")
+    @staticmethod
+    def listar_itens(lista):
+        chaves = [key for key in lista[0].keys()]
+        maiores = []
+        for chave in chaves:
+            valor_maior = 0
+            for item in lista:
+                if len(str(item[chave])) > valor_maior:
+                    valor_maior = len(str(item[chave]))
+                else:
+                    pass
+            maiores.append(valor_maior)
+        print(f" {'No':_^4} ", end="")
+        for c in range(0, len(chaves)):
+            print(f" {chaves[c].upper():_^{maiores[c]+2}} ", end="")
+        print()
+        numero = 1
+        for dic in lista:
+            print(f" {(numero):^4} ", end="")
+            for c in range(0, len(chaves)):
+                print(f" {dic[chaves[c]]:<{maiores[c]+2}} ", end="")
             print()
-            print("-="*20)
-            c += 1
-        
-    def pesquisar_time(self, termo_pesquisa):
+            numero += 1
+   
+    def pesquisar_time(self, termo_pesquisa=None):
         api = self.acesso_autenticado()
-        times = api.times(query=str(termo_pesquisa))
+        if not termo_pesquisa:
+            termo_pesquisa = str(input('Digite o nome do Time: '))
+        times = api.times(query=termo_pesquisa)
         lista_times = []
         for item in times:
             temp = {"id":item.id, "nome":item.nome, "cartoleiro":item.nome_cartola}
             lista_times.append(temp)
         return lista_times
 
-    def pesquisar_liga(self, termo_pesquisa):
+    def pesquisar_liga(self, termo_pesquisa=None):
         api = self.acesso_autenticado()
-        ligas = api.ligas(query=str(termo_pesquisa))
+        if not termo_pesquisa:
+            termo_pesquisa = str(input('Digite o nome da Liga: '))
+        ligas = api.ligas(query=termo_pesquisa)
         lista_ligas = []
         for item in ligas:
             temp = {"nome":item.nome, "slug":item.slug}
             lista_ligas.append(temp)
         return lista_ligas
 
+    @staticmethod
+    def int_input(texto=''):
+        while True:
+            try:
+                entrada = int(input(texto))
+            except ValueError:
+                print('Digite um valor válido.')
+            else:
+                break
+        return entrada
 
-
+    def escolher_entre_opcoes(self, dicionario):
+        while True:
+            escolha = self.int_input(f'Escolha uma das opções entre 1 e {len(dicionario)} e digite aqui sua escolha: ')
+            if escolha == 0:
+                return False
+            elif 0 < escolha <= len(dicionario):
+                return dicionario[escolha-1]
+            else:
+                print('Opção inválida.')
+        
         
     
--- teste.py
+++ teste.py
@@ -2,5 +2,7 @@ from funcionalidades import *
 import sqlite3
 
 x = Funcionalidades()
-y = x.pesquisar_liga("roleta")
+y = x.pesquisar_liga(123)
 x.listar_itens(y)
+#z = x.escolher_entre_opcoes(y)
+#print(z)
