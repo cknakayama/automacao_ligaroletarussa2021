da7250920c5ea7d1e79ac5243e14f9a80e838f08
Cristiano <ckn14oz@hotmail.com>
2021-12-10 23:25:58 -0300

    rodada 38

--- controle_mata-mata.py
+++ controle_mata-mata.py
@@ -2,17 +2,17 @@ from ligaroletarussa import *
 
 
 class MataMatasLiga(Pontuacao):
-tabelas = ("MataMataA", "MataMataB", "MataMataC")
+    tabelas = ("MataMataA", "MataMataB", "MataMataC")
 
-def atualizar_pontuacao(self):
-    api = self.acesso_autenticado()
-    arquivo = load_workbook('Planilha-Mata-matas.xlsx')
-    for tabela in tabelas:
-        planilha = arquivo(tabela)
-        contador = 2
-        while True:
-            id = planilha[f'C{contador}']
-            time = api.time(id=id, as_json=True)
+    def atualizar_pontuacao(self):
+        api = self.acesso_autenticado()
+        arquivo = load_workbook('Planilha-Mata-matas.xlsx')
+        for tabela in tabelas:
+            planilha = arquivo(tabela)
+            contador = 2
+            while True:
+                id = planilha[f'C{contador}']
+                time = api.time(id=id, as_json=True)
             
 
 
